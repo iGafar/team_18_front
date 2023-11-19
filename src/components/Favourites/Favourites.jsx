07bbd5801792e3fcca28bcac8ee602e5bff58fb1
RamzanTumgojev <ramzan.tumgojev@gmail.com>
2023-11-19 10:41:13 +0100

    minor clean up

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -1,7 +1,5 @@
 import "./Favourites.css";
 import React from "react";
-import arrowDown from "../../assets/images/arrown-down.svg";
-import Scenarios from "../NewsFilterSourceItem/Scenarios";
 
 export default function Favourites() {
     return (
