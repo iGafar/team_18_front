b2551054616f90ae1b1406f7b57d1a0f350a717a
RamzanTumgojev <ramzan.tumgojev@gmail.com>
2023-11-19 10:56:08 +0100

    created elements inside favourites

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -1,10 +1,27 @@
 import "./Favourites.css";
 import React from "react";
+import toggleOn from "../../assets/images/toggle-on.svg";
 
 export default function Favourites() {
     return (
-       <div>
-           Favourites Work
+       <div className="favourites-container">
+           <div className="user">
+               <div className="toggle">
+                   <img src={toggleOn}></img>
+               </div>
+               <div className="user-name">
+                   <span>Имя пользователя</span>
+               </div>
+           </div>
+
+           <div className="user">
+               <div className="toggle">
+                   <img src={toggleOn}></img>
+               </div>
+               <div className="user-name">
+                   <span>Имя пользователя</span>
+               </div>
+           </div>
        </div>
     );
 }
