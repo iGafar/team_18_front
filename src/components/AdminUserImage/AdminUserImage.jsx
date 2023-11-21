74af9c3f1604236361368151616345f102eb328f
Ant-on-git <denisenkoAA88@gmail.com>
2023-11-21 16:07:20 +0300

    add admin user image

---
+++ src/components/AdminUserImage/AdminUserImage.jsx
@@ -0,0 +1,16 @@
+import "./AdminUserImage.css"
+import default_img from "../../assets/images/Admin-default-img.png"
+
+
+export default function AdminUserImage() {
+
+    return (
+        <div className="adminUserImage-container">
+            <div className="adminUserImage-wrapper">
+                <div className="adminUserImage-image"><img src={ default_img } alt="" /></div>
+                <button>ИЗМЕНИТЬ ФОТО</button>
+            </div>
+        </div>
+
+    );
+}
